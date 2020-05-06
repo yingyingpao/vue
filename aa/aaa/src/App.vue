1b540de498aede34cca7f15b00e53dfbfc893888
“wangying <wangying>
2020-05-06 14:11:37 +0800

    iframe通信

---
+++ aa/aaa/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="act-form">
+    <iframe :src="src" ref="iframe" frameborder="0"></iframe>
+    <div>从子页面传递的数据：{{childData}}</div>
+    <div @click="sendMessage">向iframe发送信息</div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      // 通过信息传参
+      src: 'http://localhost:8080/#/'
+      // 地址栏传参
+      // src: 'http://localhost:8080/#/?aa=333',
+    }
+  },
+  methods: {
+    sendMessage () {
+      console.log('父向子传参')
+      let iframeWin = this.$refs.iframe.contentWindow;
+      iframeWin.postMessage({
+          cmd: 'getFormJson',
+          params: {}
+      }, '*');
+    },
+    handleMessage (event) {
+      console.log('父接受子传递过来的参数',event.data)
+      var data = event.data
+      switch (data.cmd) {
+          case 'returnFormJson':
+            // 处理业务逻辑
+            this.childData = data;
+            break;
+      }
+    }
+  },
+  mounted () {
+    window.addEventListener("message", this.handleMessage);
+  }
+}
+</script>
